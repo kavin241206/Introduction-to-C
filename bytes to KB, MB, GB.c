12eed9e9e3f5d6164b05a5848b94f96869feb2d4
kavin241206 <kavinkarthicksaravanan@gmail.com>
2025-08-29 01:25:16 +0530

    Add files via upload

---
+++ bytes to KB, MB, GB.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    float w, x, y, z;
+    float Bytes = w;
+    float KB = x;
+    float MB = y;
+    float GB = z;
+    w = x/0.001;
+    w = y/1000000;
+    w = z/ 1000000000;
+    printf("enter the values of KB, MB, GB");
+    scanf("%f%f%f", &x, &y, &z);
+    printf("the value in KB is %f\n", w = x/0.001);
+    printf("the value in MB is %f\n", w = y/1000000);
+    printf("the value in GB is %f", w = z/1000000000);
+    return 0;
+
+}
