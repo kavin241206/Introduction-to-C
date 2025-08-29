653c92b1b29456dbf6f1ee510b8f5f6f331296cd
kavin241206 <kavinkarthicksaravanan@gmail.com>
2025-08-29 09:30:13 +0530

    Add files via upload

---
+++ loop L3_P1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    int n = 1;
+    do {
+        printf("%d", n);
+        n++;
+       } while (n<=10);
+       n = 1;
+       while (n<=10)
+       {
+
+           printf("%d", n);
+           n++;
+       }
+}
